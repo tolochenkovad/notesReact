cec0555860e42e480ef20b439861ef6f2ae1010a
Vadim Tolochenko <bart93lol@gmail.com>
2019-08-18 19:09:01 +0300

    update textarea

--- src/components/AddNote/AddNote.jsx
+++ src/components/AddNote/AddNote.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';import { log } from 'util';
+
 import './AddNote.scss'
 
 const useInputValue = (defaultValue = '') => {
     const [value, setValue] = useState(defaultValue);
-
     return {
         bind: {
             value,
@@ -17,20 +17,56 @@ const useInputValue = (defaultValue = '') => {
 };
 
 const AddNote = ({ onCreate }) => {
-    const input = useInputValue('');
+    const textarea = useInputValue('');
+    const refTextarea = React.createRef();
+    const refBtn = React.createRef();
+    const refForm = React.createRef();
+
     const submitHandler = (e) => {
         e.preventDefault();
-        if (input.value().trim()) {
-            onCreate(input.value())
-            input.clear()
+        refTextarea.current.classList.remove('fullView');
+        if (textarea.value().trim()) {
+            onCreate(textarea.value())
+            textarea.clear()
         }
     };
+
+   
+
+    const setFullView = e => {
+        e.target.classList.add('fullView');  
+    }
+
+    const setShortView = e => {
+        e.target.classList.remove('fullView');  
+        refBtn.current.click();
+    }
+
+    const onPressEnter = e => {
+        if( e.keyCode === 13 ) {
+            e.preventDefault();
+            refBtn.current.click();
+        }
+    }
+
+    const onPressKey = e => {
+        e.target.classList.add('fullView');
+    }
 
     return (
         <div className="addNote">
             <i className="fas fa-plus" />
-            <form onSubmit={submitHandler}>
-                <input className="addNote__input" {...input.bind} />
+            <form ref={refForm} onSubmit={submitHandler}>
+
+            <textarea ref={refTextarea} 
+                onFocus={setFullView} 
+                onBlur={setShortView} 
+                onKeyDown={onPressEnter} 
+                onKeyPress={onPressKey}
+                className="addNote__textarea" 
+                {...textarea.bind} />
+
+            <button ref={refBtn} className="addNote__btn" type="submit">Add Note</button>
             </form>
         </div>
     )
