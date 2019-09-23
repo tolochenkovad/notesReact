cd6b4cf69cc405f19db82dc66edb84c4e1e13487
tolochenko vadim <vadim.tolochenko@gmail.com>
2019-09-23 09:08:15 +0300

    add npm material-ui/styles, CssBaseline, makeStyles in Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import NotesContainer from './components/NotesContainer';
 import Header from './components/Header';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 const App = () => (
     <>
+        <CssBaseline />
         <Header />
         <NotesContainer /> 
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+    title: {
+        fontFamily: '"Permanent Marker", cursive',
+        fontSize: '80px',
+        textAlign: 'center',
+        fontWeight: 900,
+        color: 'blue',
+        marginBottom: '20px'
+    }
+});
 
 const Header = () => {
+    const classes = useStyles();
     return(
-        <header className="header">
-            <h1 className="header__title">Notes App</h1>
+        <header>
+            <h1 className={classes.title}>Notes App</h1>
         </header>
     )
 };
