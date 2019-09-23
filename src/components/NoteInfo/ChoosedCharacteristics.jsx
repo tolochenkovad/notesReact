14e95a513dafb2a07b76c2fcbb6018a42cbb5b3b
tolochenko vadim <vadim.tolochenko@gmail.com>
2019-09-23 13:50:47 +0300

    add makeStyle in ChoosedCharacteristics

--- src/components/NoteInfo/ChoosedCharacteristics.jsx
+++ src/components/NoteInfo/ChoosedCharacteristics.jsx
@@ -1,27 +1,84 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import clsx from 'clsx';
+
+const theme = createMuiTheme();
+
+const useStyles = makeStyles({
+    characteristics:{
+        background: '#e0d8d8',
+        width: '60%'
+    },
+    tagsBox:{
+        margin: 0,
+        padding: theme.spacing(2.5),
+        listStyle: 'none',   
+        display: 'flex',
+        flexWrap: 'wrap'
+    },
+    choosedTag:{
+        background: '#baa4a4',
+        padding: theme.spacing(0.625, 2.5),
+        borderRadius: theme.spacing(1.875),
+        fontSize: theme.spacing(1.5),
+        marginRight: theme.spacing(1.25),
+        marginTop: theme.spacing(1.25),
+        '& span': {
+            marginLeft: theme.spacing(1.25)
+        }
+    },
+    iconDel:{
+        marginLeft: theme.spacing(1.25),
+        cursor: 'pointer'
+    },
+    categoryBox:{
+        margin: 0,
+        padding: theme.spacing(0, 2.5, 2.5, 2.5),
+        listStyle: 'none',   
+        display: 'flex',
+        flexWrap: 'wrap'
+    },
+    choosedСategory:{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: theme.spacing(0.625, 1.25),
+        border: '1px solid black',
+        marginRight: theme.spacing(2.5),
+        fontSize: theme.spacing(1.5),
+        marginTop: theme.spacing(1.25),
+        cursor: 'pointer'
+    }
+});
+
+
 
 const ChoosedCharacteristics = ({tagsArrNote, categoryArrNote, delTag, delCategory}) => {
+
+    const classes = useStyles();
+
     return(
-        <div className="noteInfo__characteristics">
-            <ul className="noteInfo__tags-container">
+        <div className={classes.characteristics}>
+            <ul className={classes.tagsBox}>
                 {
                     tagsArrNote.map(item =>
-                        <li key={item.id} className="noteInfo__choosed-tag">
+                        <li key={item.id} className={classes.choosedTag}>
                             <i className="fas fa-paperclip fa-xs" />
                             <span>{item.tag}</span>
-                            <i onClick={() => delTag(item.id)} className="info__icon-del fas fa-times" />
+                            <i onClick={() => delTag(item.id)} className={clsx(classes.iconDel, 'fas fa-times')} />
                         </li>
                     )
                 }
             </ul>
 
-            <ul className="noteInfo__category-container">
+            <ul className={classes.categoryBox}>
                 {
                     categoryArrNote.map(item =>
-                        <li key={item.id} className="category__text noteInfo__category-item">
+                        <li key={item.id} className={classes.choosedСategory}>
                             {item.category}
-                            <i onClick={() => delCategory(item.id)} className="info__icon-del fas fa-times" />
+                            <i onClick={() => delCategory(item.id)} className={clsx(classes.iconDel, 'fas fa-times')} />
                         </li>
                     )
                 }
