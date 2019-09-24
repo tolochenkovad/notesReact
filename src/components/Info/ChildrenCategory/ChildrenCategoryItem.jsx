307587c696d2d9b04683b1ab1b926cce4947d181
tolochenko vadim <vadim.tolochenko@gmail.com>
2019-09-24 15:05:07 +0300

    add ChildrenCategoryItem

---
+++ src/components/Info/ChildrenCategory/ChildrenCategoryItem.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useStyle } from './style';
+import clsx from 'clsx';
+import EditElement from '../EditElement/EditElement';
+import Grid from '@material-ui/core/Grid';
+
+const ChildrenCategoryItem = ({getActiveFilterCategory, onEditCategory, isEditIcon, currentIdCategory,
+    categoryValue, addCategory, onBlurFunc, removeCategory, child}) => {
+
+    const classes = useStyle();
+
+    return(
+        <Grid onClick={getActiveFilterCategory} className={classes.category}>
+            <i onClick={() => onEditCategory(child.id, child.categoryValue)} className={clsx(classes.iconEdit, 'fas fa-edit')} />
+            {
+                isEditIcon && currentIdCategory === child.id
+                    ? 
+                        <EditElement elementValue={categoryValue} id={child.id} addElement={addCategory} onBlurFun={onBlurFunc} />
+                    : 
+                        child.categoryValue
+            }
+            <i onClick={() => removeCategory(child.id, child.categoryValue)} className={clsx(classes.iconDel, 'fas fa-times')} />
+         </Grid> 
+    )
+};
+
+ChildrenCategoryItem.propTypes = {
+    isEditIcon: PropTypes.bool,
+    onBlurFunc: PropTypes.func,
+    removeCategory: PropTypes.func,
+    onEditCategory: PropTypes.func,
+    addCategory: PropTypes.func,
+    categoryValue: PropTypes.string,
+    currentIdCategory: PropTypes.number,
+    getActiveFilterCategory: PropTypes.func,
+    child: PropTypes.object
+}
+
+export default ChildrenCategoryItem;
