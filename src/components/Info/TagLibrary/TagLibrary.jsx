89a9f4d4651436aba1c07bdd5847bd7479db5810
tolochenko vadim <vadim.tolochenko@gmail.com>
2019-09-24 15:31:03 +0300

    refactoring TagLibrary

--- src/components/Info/TagLibrary/TagLibrary.jsx
+++ src/components/Info/TagLibrary/TagLibrary.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import EditElement from '../EditElement/EditElement';
-import clsx from 'clsx';
 import { useStyles } from './style';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import TagItem from './TagItem';
 
 const TagLibrary = ({tags, getActiveFilterTag, onEditTag, isTagEdit, 
     currentIdTag, tagValue, removeTag, addTag, onBlurFunc}) => {
@@ -11,38 +12,31 @@ const TagLibrary = ({tags, getActiveFilterTag, onEditTag, isTagEdit,
 
     return(
         <>
-            <h2 className={classes.headline}>Tag library</h2>
-                <ul className={classes.tagsList}>
+            <Typography variant="h2" className={classes.headline}>Tag library</Typography>
+                <List className={classes.tagsList}>
                     {
                         tags.map( (tag, index) =>
-                            <li onClick={getActiveFilterTag} key={index} className={classes.tag}>
-                                <i onClick={() => onEditTag(tag.id, tag.tag)} className={clsx(classes.iconEdit, 'fas fa-edit')} />
-                                {
-                                    isTagEdit && currentIdTag === tag.id
-                                        ? 
-                                            <EditElement elementValue={tagValue} id={tag.id} addElement={addTag} onBlurFun={onBlurFunc} />
-                                        : 
-                                            tag.tag
-                                }
-                                <i onClick={() => removeTag(tag.id, tag.tag)} className={clsx(classes.iconDel, 'fas fa-times')} />
-                            </li>   
+                            <TagItem    getActiveFilterTag={getActiveFilterTag}
+                                        onEditTag={onEditTag}
+                                        isTagEdit={isTagEdit}
+                                        currentIdTag={currentIdTag}
+                                        tagValue={tagValue}
+                                        addTag={addTag}
+                                        onBlurFunc={onBlurFunc}
+                                        removeTag={removeTag}
+                                        key={index}
+                                        tag={tag}
+                                
+                            />  
                         )
                     }
-                </ul>
+                </List>
         </>
     )
 };
 
 TagLibrary.propTypes = {
-    tags: PropTypes.arrayOf(PropTypes.object),
-    getActiveFilterTag: PropTypes.func,
-    isTagEdit: PropTypes.bool,
-    currentIdTag: PropTypes.number,
-    onEditTag: PropTypes.func,
-    removeTag: PropTypes.func,
-    tagValue: PropTypes.string,
-    addTag: PropTypes.func,
-    onBlurFunc: PropTypes.func
+    tags: PropTypes.arrayOf(PropTypes.object)
 }
 
 export default TagLibrary;
