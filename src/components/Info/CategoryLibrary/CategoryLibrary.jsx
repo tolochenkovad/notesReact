15cf95a37132883fd2f967e5dd7cc2678228fbe1
tolochenko vadim <vadim.tolochenko@gmail.com>
2019-09-24 15:07:23 +0300

    refactoring CategoryLibrary

--- src/components/Info/CategoryLibrary/CategoryLibrary.jsx
+++ src/components/Info/CategoryLibrary/CategoryLibrary.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import EditElement from '../EditElement/EditElement';
 import ChildrenCategory from '../ChildrenCategory/ChildrenCategory';
-import clsx from 'clsx';
 import { useStyle } from './style';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import CategoryItem from './CategoryItem';
 
 const CategoryLibrary = ({tree, getActiveFilterCategory, onEditCategory, isEditIcon, currentIdCategory,
     categoryValue, addCategory, onBlurFunc, removeCategory}) => {
@@ -12,62 +14,51 @@ const CategoryLibrary = ({tree, getActiveFilterCategory, onEditCategory, isEditI
 
     return (
         <>
-            <h3 className={classes.headline}>Category library</h3>
-                <ul className={classes.categoryList}>
+            <Typography variant="h3" className={classes.headline}>Category library</Typography>
+                <List className={classes.categoryList}>
                     {
                         tree.map( item => item.children && item.parent === null 
                             ?
-                                <li className={classes.item} key={item.id}>
-                                    <span onClick={getActiveFilterCategory} className={classes.category} >
-                                        <i onClick={() => onEditCategory(item.id, item.categoryValue)} className={clsx(classes.iconEdit, 'fas fa-edit')} />
-                                        {
-                                        isEditIcon && currentIdCategory === item.id
-                                            ? 
-                                                <EditElement elementValue={categoryValue} id={item.id} addElement={addCategory} onBlurFun={onBlurFunc} />
-                                            : 
-                                                item.categoryValue
-                                        }
-
-                                        <i onClick={() => removeCategory(item.id, item.categoryValue)} className={clsx(classes.iconDel, 'fas fa-times')} />
-                                    </span>
+                                <ListItem className={classes.item} key={item.id}>
+                                    <CategoryItem getActiveFilterCategory={getActiveFilterCategory} 
+                                                  onEditCategory={onEditCategory}
+                                                  isEditIcon={isEditIcon}
+                                                  currentIdCategory={currentIdCategory}
+                                                  categoryValue={categoryValue}
+                                                  addCategory={addCategory}
+                                                  onBlurFunc={onBlurFunc}
+                                                  removeCategory={removeCategory}
+                                                  item={item}
+                                    />
                                     <ChildrenCategory children={item.children} removeCategory={removeCategory} 
                                     isEditIcon={isEditIcon} getActiveFilterCategory={getActiveFilterCategory} 
                                     addCategory={addCategory} onBlurFunc={onBlurFunc} categoryValue={categoryValue} 
                                     currentIdCategory={currentIdCategory} onEditCategory={onEditCategory} />
-                                </li>
-                            :   !item.children && item.parent === null
+                                </ListItem>
+                            :   
+                            !item.children && item.parent === null
                                 ? 
-                                    <li className={classes.item} key={item.id}>
-                                        <span onClick={getActiveFilterCategory} className={classes.category} >
-                                        <i onClick={() => onEditCategory(item.id, item.categoryValue)} className={clsx(classes.iconEdit, 'fas fa-edit')} />
-                                        {
-                                        isEditIcon && currentIdCategory === item.id
-                                            ? 
-                                                <EditElement elementValue={categoryValue} id={item.id} addElement={addCategory} onBlurFun={onBlurFunc} />
-                                            : 
-                                                item.categoryValue
-                                        }
-                                        <i onClick={() => removeCategory(item.id, item.categoryValue)} className={clsx(classes.iconDel, 'fas fa-times')} />
-                                        </span>
-                                    </li>
+                                    <ListItem className={classes.item} key={item.id}>
+                                        <CategoryItem getActiveFilterCategory={getActiveFilterCategory} 
+                                                  onEditCategory={onEditCategory}
+                                                  isEditIcon={isEditIcon}
+                                                  currentIdCategory={currentIdCategory}
+                                                  categoryValue={categoryValue}
+                                                  addCategory={addCategory}
+                                                  onBlurFunc={onBlurFunc}
+                                                  removeCategory={removeCategory}
+                                                  item={item}
+                                        />
+                                    </ListItem>
                                 : null
                         )
                     }
-                </ul>
+                </List>
         </>
     )
 };
 
 CategoryLibrary.propTypes = {
-    getActiveFilterCategor: PropTypes.func,
-    onEditCategory: PropTypes.func,
-    isEditIcon: PropTypes.bool,
-    onBlurFunc: PropTypes.func,
-    addCategory: PropTypes.func,
-    removeCategory: PropTypes.func,
-    categoryValue: PropTypes.string,
-    currentIdCategory: PropTypes.number,
-    editCategory: PropTypes.func,
     tree: PropTypes.arrayOf(PropTypes.object)
 };
 
