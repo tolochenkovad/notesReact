a59353a0f80ef6d99f7b7c03560a0c14bc65c5e5
tolochenko vadim <vadim.tolochenko@gmail.com>
2019-10-03 13:47:34 +0300

    add functional disabled trigger on buttons

--- src/app/Header/Header.jsx
+++ src/app/Header/Header.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
@@ -25,19 +26,39 @@ const useStyles = makeStyles( theme => ({
 
 const Header = ({changeLocale}) => {
     const classes = useStyles();
+
+    const [isBtnEn, setBtnEn] = useState(true);
+    const [isBtnRu, setBtnRu] = useState(false);
+
+    const handleClickBtn = (language) => {
+        changeLocale(language);
+        if (language === 'en'){
+            setBtnEn(true);
+            setBtnRu(false)
+        } else {
+            setBtnRu(true);
+            setBtnEn(false);
+        }
+    };
+
+
     return(
         <Grid className={classes.container}>
             <Typography variant="h1" className={classes.title}>
                 <FormattedMessage id="homeComponent.title" defaultMessage="Default message" />
             </Typography>
-            <Button variant="outlined" className={classes.btn} onClick={ () => changeLocale('en') }>
+            <Button disabled={isBtnEn} variant="outlined" className={classes.btn} onClick={ () => handleClickBtn('en') }>
                 <FormattedMessage id="homeComponent.english" defaultMessage="Default message" />
             </Button>
-            <Button variant="outlined" className={classes.btn} onClick={ () => changeLocale('ru') }>
+            <Button disabled={isBtnRu} variant="outlined" className={classes.btn} onClick={ () => handleClickBtn('ru') }>
                 <FormattedMessage id="homeComponent.russian" defaultMessage="Default message" />
             </Button>
         </Grid>
     )
+};
+
+Header.propTypes = {
+    changeLocale: PropTypes.func
 };
 
 export default Header;
