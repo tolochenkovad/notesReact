203e9672878b5a3c48f5709155f5a3a72bf7c391
tolochenko vadim <vadim.tolochenko@gmail.com>
2019-10-08 13:58:45 +0300

    refactoring tags redux

--- src/app/InfoPage/redux-tags/saga.js
+++ src/app/InfoPage/redux-tags/saga.js
@@ -5,36 +5,34 @@ import {
     REMOVE_TAG,
     REMOVE_TAG_SAGA
 } from "./constants";
-import { takeLatest, put, select } from "redux-saga/effects";
+import {takeLatest, put, select} from "redux-saga/effects";
 import {setStorage} from "../../../utils/localStorage";
 import {getTags, getTagsOfNote} from "./selectors";
 
 function* addTag(action) {
     const tags = yield select(getTags);
-    debugger;
-    if ( tags.some(item => item.id === action.id || item.tag === action.tag )) {
-        debugger;
-        yield put({ type: CHANGE_CURRENT_TAG, action});
+    if (tags.some(item => item.id === action.payload.id || item.tag === action.payload.tag)) {
+        yield put({type: CHANGE_CURRENT_TAG, action});
         const tags2 = yield select(getTags);
         setStorage("tags", tags2);
         return;
     }
-    yield put({ type: ADD_TAG, action});
+    yield put({type: ADD_TAG, action});
     const tags3 = yield select(getTags);
     setStorage("tags", tags3);
 }
 
 function* addTagOfNote(action) {
     const tagsArrNote = yield select(getTagsOfNote);
-    if ( tagsArrNote.some(item =>  item.tag === action.tag) ) {
+    if (tagsArrNote.some(item => item.tag === action.payload)) {
         alert('This tags is already added!');
         return;
     }
-    yield put({ type: ADD_TAG_OF_NOTE, action});
+    yield put({type: ADD_TAG_OF_NOTE, action});
 }
 
 function* removeTag(action) {
-    yield put({ type: REMOVE_TAG, action});
+    yield put({type: REMOVE_TAG, action});
     const tags = yield select(getTags);
     setStorage("tags", tags);
 }
